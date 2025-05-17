d64557b9f9112e6d32dc363d7f7f0dbe93ffaf68
Potte8 <henry.podssuweit@gmx.de>
2025-05-18 01:07:06 +0200

    Create ghost_main.c

---
+++ src/ghost_main.c
@@ -0,0 +1,11 @@
+#include "debug.h"
+
+int main(void) {
+    debug_init();
+
+    debug_log(DEBUG_LEVEL_INFO, "GhostOS gestartet");
+    debug_log(DEBUG_LEVEL_WARN, "GPS Modul nicht gefunden");
+    debug_log(DEBUG_LEVEL_ERROR, "WiFi Scan Fehler in module_wifi.c:118");
+
+    return 0;
+}
